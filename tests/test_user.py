b6a869dd77bc5a7f9198f9163bd8baf3dcec76e2
Agnes <koinageagness@gmail.com>
2022-05-08 11:51:05 +0300

    import render_templte and auth blueprint in auth/views.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,9 +8,19 @@ class UserTest(unittest.TestCase):
     def test_init(self):
         self.assertTrue(self.new_user.password_encrypt is not None)
 
+    # def test_password_setter(self):
+    #     self.assertTrue(self.new_user.pass_secure is not None)
+
     def test_alert(self):
         with self.assertRaises(AttributeError):
             self.new_user.password
+    
+    # def test_no_access_password(self):
+    #         with self.assertRaises(AttributeError):
+    #             self.new_user.password
+
+    # def test_password_verification(self):
+    #         self.assertTrue(self.new_user.verify_password('banana'))
 
     def test_save(self):
         self.assertTrue(self.new_user.check_password('agnes1234'))
