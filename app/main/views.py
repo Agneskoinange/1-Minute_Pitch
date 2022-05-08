fd29600d23dbfe17fabe2d7ca94fe93e9e6f65bd
Agnes <koinageagness@gmail.com>
2022-05-08 22:32:05 +0300

    create a profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from flask import render_template,request,redirect,url_for,abort
 from flask import render_template
 from pip import main
 from app import app
@@ -30,3 +31,18 @@ def pitch_form():
         new_pitch.save_pitch()
         # return redirect(url_for('main.home'))
     return render_template('new_pitch.html', pitch_form=pitch_form, )
+
+@main.route('/user/<name>', methods=['GET','POST'])
+@login_required
+def profile(name):
+    user = User.query.filter_by(username=name).first()
+    if user is None:
+        abort(404)
+
+    form=EditProfile()
+    if form.validate_on_submit():
+        user.about=form.about.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile', name=user.username))
+    return render_template('profile/profile.html', user=user, form=form)
