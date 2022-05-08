0f3b112a6781818517fa8233ac42f949148a9781
Agnes <koinageagness@gmail.com>
2022-05-08 21:10:13 +0300

    add decorator @login_manage.user_loader

--- app/models.py
+++ app/models.py
@@ -3,6 +3,11 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
@@ -10,7 +15,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     about = db.Column(db.String(255))
     avatar = db.Column(db.String())
-    
     password_encrypt=db.Column(db.String(128))
     
     @property   #write-only
