006d959ea19be9124407e4a96a9a368a23e39b73
Agnes <koinageagness@gmail.com>
2022-05-08 10:13:37 +0300

    register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,15 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
 
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_object(config_options[config_name])
+
+
 
 
 from app import views
