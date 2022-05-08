95dc1328be88905833e70795c898172755538b8b
Agnes <koinageagness@gmail.com>
2022-05-08 09:20:01 +0300

    set configurations and import sql,alchemy,bosstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+
+    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
 
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_object(config_options[config_name])
 
 
 from app import views
