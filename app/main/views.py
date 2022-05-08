800e497edd140c5aa99dfbb071cf444af12ac5cf
Agnes <koinageagness@gmail.com>
2022-05-08 21:05:06 +0300

    add viws rout for new_pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from pip import main
 from app import app
-from flask_login import login_required
+from flask_login import current_user, login_required
+from ..models import User, Pitches
 
 # Views
 @main.route('/')
@@ -10,13 +11,22 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    pitches=Pitches.query.all()
+    identification = Pitches.user_id
+    posted_by = User.query.filter_by(id=identification).first()
+    user = User.query.filter_by(id=current_user.get_id()).first()
+
+    return render_template('index.html', pitches=pitches, posted_by=posted_by, user=user)
 
 
-@main.route('/pitches')
-def pitches():
-
-    '''
-    View Pitch page function that returns the Pitch details page and its data
-    '''
-    return render_template('pitches.html', pitches=pitches)
+@main.route('/new_pitch', methods=['GET','POST'])
+@login_required
+def pitch_form():
+    # pitch_form = PitchForm()
+    if pitch_form.validate_on_submit():
+        category=pitch_form.pitch_category.data
+        text = pitch_form.pitch_text.data
+        new_pitch = Pitches(category=category, text=text, user=current_user)
+        new_pitch.save_pitch()
+        # return redirect(url_for('main.home'))
+    return render_template('new_pitch.html', pitch_form=pitch_form, )
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
