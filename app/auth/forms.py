25fd7a5a946b4f4987c62405b68d0a737f503fd2
Agnes <koinageagness@gmail.com>
2022-05-08 21:18:03 +0300

    add custom validtors

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField, BooleanField ,ValidationError
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
@@ -10,3 +10,11 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('A user with that email address exists')
+    
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('THis username is already taken')
