2baa3713c72ed8e2bc9bce7ae4f7705ead34c6cc
Agnes <koinageagness@gmail.com>
2022-05-08 10:08:29 +0300

    use the @manager.command decorator  create the test function that loads all the test files and runs them all

--- manage.py
+++ manage.py
@@ -9,9 +9,16 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
-    
+
 if __name__ == '__main__':
     app.run()
