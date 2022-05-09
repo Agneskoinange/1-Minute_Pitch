36dbb3a12edb5c4c655a182ec7edaf6f4dc6a319
Agnes <koinageagness@gmail.com>
2022-05-09 03:02:47 +0300

    add viw functions for comments and categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,11 @@
 from flask import render_template,request,redirect,url_for,abort
-from flask import render_template
 from pip import main
-from app import app
+# from app import app
 from flask_login import current_user, login_required
-from ..models import User, Pitches
+from ..models import User, Pitches, Comments, UpVote
 from .forms import EditProfile, PitchForm, CommentForm
 from .. import db, photos
-import markdown2  
+# import markdown2  
 
 # Views
 @main.route('/')
@@ -48,7 +47,7 @@ def profile(name):
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('.profile', name=user.username))
-    return render_template('profile/profile.html', user=user )
+    return render_template('profile/profile.html', user=user, form=form )
 
 @main.route('/user/<name>/edit/pic', methods=['POST'])
 @login_required
@@ -60,3 +59,32 @@ def update_pic(name):
         user.avatar=path
         db.session.commit()
     return redirect(url_for('main.profile', name=name))
+
+
+@main.route('/categories/<pitch_category>')
+def categories(pitch_category):
+    pitch = Pitches.get_category(pitch_category)
+
+    identification = Pitches.user_id
+    posted_by = User.query.filter_by(id=identification).first()
+    return render_template('categories.html', pitch=pitch, posted_by=posted_by)
+
+
+@main.route('/comments/<int:pitch_id>', methods=['GET','POST'])
+@login_required
+def pitch_comments(pitch_id):
+    comments = Comments.get_comments(pitch_id)
+
+    pitch = Pitches.query.get(pitch_id)
+    pitch_posted_by = pitch.user_id
+    user = User.query.filter_by(id=pitch_posted_by).first()
+
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = form.pitch_comment.data      
+        new_comment = Comments(comment=comment, pitch_id=pitch_id, user_id=current_user.get_id())
+        new_comment.save_comment()
+        return redirect(url_for('main.pitch_comments',pitch_id = pitch_id))
+
+    return render_template('comments.html', comment_form=form, comments=comments, pitch = pitch, user=user)
+
