ca9204d4fe3df335c87f14aeb8d1b0fb3dadecc0
Agnes <koinageagness@gmail.com>
2022-05-10 14:51:32 +0300

    revert git

--- manage.py
+++ manage.py
@@ -1,4 +1,9 @@
-from flask import Flask
+# from multiprocessing import managers
+# from app import app
+from app import create_app,db
+from app.models import User, Pitches
+from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 
