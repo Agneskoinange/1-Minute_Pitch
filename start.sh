6e99be654f9f33d19fc61bc7dbba8915dc66aba1
Agnes <koinageagness@gmail.com>
2022-05-13 01:06:59 +0300

    change app to development mode

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export FLASK_APP='app:create_app(config_name)'
 export MAIL_USERNAME='koinangeagnes@gmail.com'
 export MAIL_PASSWORD='0708042343'
-export FLASK_CONFIG='development'
+# export FLASK_CONFIG='development'
 export FLASK_APP='manage.py'
 # export FLASK_APP = "manage.py"
 # export APP_SETTINGS="development"
