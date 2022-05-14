1b0f0a0642ff79dda6d3f70385efbd7cc1187e98
Agnes <koinageagness@gmail.com>
2022-05-14 06:29:03 +0300

    delete cofig files from init.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from config import Config
-from config import DevConfig
+# from config import Config
+# from config import DevConfig
 from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'string'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
-app.config.from_object(Config)
+# app.config.from_object(Config)
 db = SQLAlchemy(app)
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
@@ -22,7 +22,6 @@ mail = Mail()
 # Initializing application
 def create_app(config_name):
     app = Flask(__name__)
-
     
     # Initializing flask extensions
     bootstrap.init_app(app)
@@ -41,13 +40,13 @@ def create_app(config_name):
 
 
     # Setting up configuration
-    app.config.from_object(Config)
-    app.config.from_object(DevConfig)
+    # app.config.from_object(Config)
+    # app.config.from_object(DevConfig)
     app.config.from_object(config_options[config_name])
-    app.config['SQLALCHEMY_DATABASE_URL'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://nessie:agnes1234@localhost/pitches'
-    app.config["SECRET_KEY"] = 'agnes12345'
+    # app.config['SQLALCHEMY_DATABASE_URL'] = os.getenv('DATABASE_URL')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://nessie:agnes1234@localhost/pitches'
+    # app.config["SECRET_KEY"] = 'agnes12345'
    
 
     # configure UploadSet
